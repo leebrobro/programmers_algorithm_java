6715648786b4d417e08949aa2d13c0c878151b72
leebrobro <ssgodda@naver.com>
2022-07-14 20:03:08 +0900

    Create programmersjavatest.java

---
+++ programmersjavatest.java
@@ -0,0 +1,17 @@
+class programmersjavatest {
+    public static void main(String[] args) {
+
+        int n = 0;
+        int answer = 0;
+        int present = n;
+        int i = 2;
+        while (i != (n + 1)) {
+
+            if (present % i == 0) {
+                answer += i;
+            }
+            i++;
+        }
+        System.out.println(answer + 1);
+    }
+}
